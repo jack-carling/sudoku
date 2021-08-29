1498c1c0702617342614be7b0e5f8233cd73345f
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-08-29 08:52:04 +0200

    Update Board
    • Added squares with event handlers and prevent defaults if input is non-digits
    • Removed appearance and caret from input style
    • Yellow background color on input focus
    • Check for duplicates on row and col
    • Red background on input if digit is a duplicate

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import './Board.scss';
 import Square from './Square';
@@ -6,13 +6,16 @@ import Square from './Square';
 interface BoardData {
   number: string;
   editable: boolean;
+  error: boolean;
 }
 
 function Board() {
-  const board: BoardData[] = [];
+  const initial: BoardData[] = [];
   for (let i = 0; i < 81; i++) {
-    board.push({ number: '', editable: true });
+    initial.push({ number: '', editable: true, error: false });
   }
+
+  const [board, setBoard] = useState(initial);
 
   const boardRef = useRef<HTMLElement>(null);
 
@@ -29,10 +32,83 @@ function Board() {
 
   window.addEventListener('resize', resizeBoard);
 
+  function handleNumber(value: string, index: number) {
+    const update = [...board];
+    update[index].number = value;
+    setBoard(update);
+    checkBoard(index);
+  }
+
+  function handleDelete(index: number) {
+    const update = [...board];
+    update[index].number = '';
+    setBoard(update);
+    checkBoard(index);
+  }
+
+  function checkBoard(index: number) {
+    const row = Math.floor(index % 9);
+    const col = Math.floor(index / 9);
+
+    let checks: string[] = [];
+    let duplicate: boolean;
+
+    // Check row
+    for (let i = col * 9; i < col * 9 + 9; i++) {
+      if (board[i].number) checks.push(board[i].number);
+    }
+
+    duplicate = checkDuplicates(index, checks);
+    if (duplicate) return;
+    checks = [];
+
+    // Check col
+    for (let i = row; i < 81; i += 9) {
+      if (board[i].number) checks.push(board[i].number);
+    }
+
+    duplicate = checkDuplicates(index, checks);
+    if (duplicate) return;
+    checks = [];
+
+    // Check box
+    let box = Math.floor(row / 3);
+    if (col >= 3 && col <= 5) box += 3;
+    if (col >= 6 && col <= 8) box += 6;
+
+    const boxIndexes = [
+      [0, 1, 2, 9, 10, 11, 18, 19, 20],
+      [3, 4, 5, 12, 13, 14, 21, 22, 23],
+      [6, 7, 8, 15, 16, 17, 24, 25, 26],
+      [27, 28, 29, 36, 37, 38, 45, 46, 47],
+      [30, 31, 32, 39, 40, 41, 48, 49, 50],
+      [33, 34, 35, 42, 43, 44, 51, 52, 53],
+      [54, 55, 56, 63, 64, 65, 72, 73, 74],
+      [57, 58, 59, 66, 67, 68, 75, 76, 77],
+      [60, 61, 62, 69, 70, 71, 78, 79, 80],
+    ];
+  }
+
+  function checkDuplicates(index: number, checks: string[]) {
+    const duplicates = checks.filter((number) => board[index].number === number).length > 1;
+
+    if (duplicates) {
+      const update = [...board];
+      update[index].error = true;
+      setBoard(update);
+      return true;
+    } else {
+      const update = [...board];
+      update[index].error = false;
+      setBoard(update);
+      return false;
+    }
+  }
+
   return (
     <section ref={boardRef} className="Board">
       {board.map((square, index) => {
-        return <Square key={index} />;
+        return <Square {...square} index={index} handleNumber={handleNumber} handleDelete={handleDelete} key={index} />;
       })}
     </section>
   );
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -2,8 +2,39 @@ import React from 'react';
 
 import './Square.scss';
 
-function Square() {
-  return <div className="Square">0</div>;
+interface Props {
+  error: boolean;
+  index: number;
+  handleNumber: (input: string, index: number) => void;
+  handleDelete: (index: number) => void;
+}
+
+function Square({ error, index, handleNumber, handleDelete }: Props) {
+  function handleInput(e: React.KeyboardEvent<HTMLInputElement>) {
+    const valid = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9'];
+
+    if (!valid.includes(e.code)) {
+      e.preventDefault();
+      return;
+    }
+    handleNumber(e.key, index);
+    e.currentTarget.value = e.key;
+  }
+  function handleRemoveInput(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.code === 'Backspace') handleDelete(index);
+  }
+
+  return (
+    <div className="Square">
+      <input
+        className={error ? 'error' : ''}
+        onKeyPress={handleInput}
+        onKeyUp={handleRemoveInput}
+        type="tel"
+        maxLength={1}
+      />
+    </div>
+  );
 }
 
 export default Square;
